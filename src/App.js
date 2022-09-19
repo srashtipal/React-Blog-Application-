ea988d01daf6e90af00b02aa1d803358adc87bf6
srashtipal <srashtipal64@.com>
2022-09-19 20:02:45 +0530

    Changes done in main component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <BrowserRouter>
-    <ToastContainer/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/services" element={<Services/>}/>
-      <Route path="/user" element={<Privateroute/>}>
-      <Route path="dashboard" element={<Userdashboard/>}/> </Route>
-
-    </Routes>
-    </BrowserRouter>
+    <div>
+      This is home
+    </div>
   );
 }
 
