3b9503dd5774633ddbadae41764b17bc2f3a187e
srashtipal <srashtipal64@.com>
2022-09-28 15:50:18 +0530

    Login & Signup UI

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,80 @@
-import Base from "../Components/Base";
+import {
+    Label,
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
+    Container,
+    Form,
+    FormGroup,
+    Input,
+    Row,
+    Button,
+  } from "reactstrap";
+  import Base from "../Components/Base";
 
 const Login=()=>{
     return(
 <Base>
-    <h1>This is Login page</h1>
-    <p>Welcome to Login page</p>
+    <Container>
+    <Row className="mt-4">
+          <Col
+            sm={{
+              size: 6,
+              offset: 3,
+            }}
+          >
+            <Card >
+              <CardHeader className="text-center">
+                <h3>Login Here !!</h3>
+              </CardHeader>
+
+              <CardBody>
+                <Form >
+                  {/* Email field */}
+
+                  <FormGroup>
+                    <Label for="email">Enter Email</Label>
+                    <Input
+                      type="text"
+                      placeholder="Enter email"
+                      id="email"
+                    
+                    />
+                  </FormGroup>
+
+                  {/* password field */}
+
+                  <FormGroup>
+                    <Label for="password">Enter password</Label>
+                    <Input
+                      type="password"
+                      placeholder="Enter password"
+                      id="password"
+                    
+                    />
+                  </FormGroup>
+
+                  <Container className="text-center">
+                    <Button color="dark" >
+                      Login
+                    </Button>
+                    <Button
+
+                      className="ms-2"
+                      outline
+                      color="secondary"
+                    >
+                      Reset
+                    </Button>
+                  </Container>
+                </Form>
+              </CardBody>
+            </Card>
+            
+            </Col>
+        </Row>
+    </Container>
 </Base> 
    )
 }
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,10 +1,102 @@
-import Base from "../Components/Base";
+import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
+    Container,
+    Form,
+    FormFeedback,
+    FormGroup,
+    Input,
+    Label,
+    Row,
+  } from "reactstrap";import Base from "../Components/Base";
 
 const Signup=()=>{
     return(
 <Base>
-<h1>This is Signup page</h1>
-    <p>Welcome to Signup page</p>
+<Container>
+<Row className="mt-4">
+          {/* { JSON.stringify(data) } */}
+
+          <Col sm={{ size: 6, offset: 3 }}>
+            <Card >
+              <CardHeader className="text-center">
+                <h3 > Fill Information to Register !!</h3>
+              </CardHeader>
+
+              <CardBody>
+                {/*creating form*/}
+
+                <Form>
+                    {/*Name Field*/ }
+                    <FormGroup>
+                        <Label for="name">Enter Name </Label>
+                        <Input
+                        type="text"
+                        placeholder="Enter here"
+                        invalid="true"
+                        id="name"
+                        />
+                    </FormGroup>
+
+                    {/*Email Field*/ }
+                    <FormGroup>
+                        <Label for="name">Enter Email </Label>
+                        <Input
+                        type="text"
+                        placeholder="Enter here"
+                        invalid="true"
+                        id="email"
+                        />
+                    </FormGroup>
+
+                    {/*Password Field*/ }
+                    <FormGroup>
+                        <Label for="name">Enter Password</Label>
+                        <Input
+                        type="password"
+                        placeholder="Enter here"
+                        invalid="true"
+                        id="password"
+                        />
+                    </FormGroup>
+
+                    {/*About Field*/ }
+                    <FormGroup>
+                        <Label for="name">Enter About</Label>
+                        <Input
+                        type="textarea"
+                        placeholder="Enter here"
+                        id="name"
+                        invalid="true"
+                        style={{ height: "250px" }}
+                        />
+                    </FormGroup>
+
+                    {/*Button */}
+                    <FormGroup>
+                    <Container className="text-center">
+                    <Button color="dark">
+                      Register
+                    </Button>
+                    <Button
+                    
+                      color="secondary"
+                      type="reset"
+                      className="ms-2"
+                    >
+                      Reset
+                    </Button>
+                  </Container>
+                    </FormGroup>
+                </Form>
+              </CardBody>
+    </Card>
+        </Col>
+    </Row>
+</Container>
 </Base>
    )
 }
