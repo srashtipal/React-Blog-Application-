6efe43544bb7225be2f1d6201ce7b70090f09fb4
srashtipal <srashtipal64@.com>
2022-10-08 22:06:45 +0530

    Profile info

---
+++ src/Pages/User-routes/ProfileInfo.jsx
@@ -0,0 +1,13 @@
+
+import Base from '../../Components/Base'
+
+function ProfileInfo() {
+  return (
+    <Base>
+      <div>ProfileInfo</div>
+      <h1>Welcome </h1>
+    </Base>
+  )
+}
+
+export default ProfileInfo;
